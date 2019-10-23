dd068ffa7c17795f44f7320c9fbdc16823d195ba
XuZeJun <xuzejun@126.com>
2019-10-23 16:27:07 +0800

    提交测试

--- gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.atguigu.gmall.user.controller;
 
 import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
 import com.atguigu.gmall.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,6 +15,13 @@ import java.util.List;
 public class UserController {
     @Autowired
     UserService userService;
+    @RequestMapping("getReceiveAddressByMemberId")
+    @ResponseBody
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId){
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = userService.getReceiveAddressByMemberId(memberId);
+        return umsMemberReceiveAddresses;
+    }
+
     @RequestMapping("getAllUser")
     @ResponseBody
     public List<UmsMember> getAllUser(){
--- gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.atguigu.gmall.user.service.impl;
 
 import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.user.mapper.UmsMemberReceiveAddressMapper;
 import com.atguigu.gmall.user.mapper.UserMapper;
 import com.atguigu.gmall.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +14,27 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     @Autowired
     UserMapper userMapper;
+    @Autowired
+    UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
 
     @Override
     public List<UmsMember> getAllUser() {
-        List<UmsMember> umsMemberList = userMapper.selectAllUser();
+        List<UmsMember> umsMemberList = userMapper.selectAll();//userMapper.selectAllUser();
         return umsMemberList;
     }
+
+    @Override
+    public List<UmsMember> getReceiveAddressByMemberId() {
+        return null;
+    }
+
+    @Override
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
+        UmsMemberReceiveAddress umsMemberReceiveAddress = new UmsMemberReceiveAddress();
+        umsMemberReceiveAddress.setMemberId(memberId);
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.select(umsMemberReceiveAddress);
+
+        return umsMemberReceiveAddresses;
+    }
+
 }
