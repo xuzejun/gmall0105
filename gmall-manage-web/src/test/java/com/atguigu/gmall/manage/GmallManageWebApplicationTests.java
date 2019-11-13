c5f76f9fd2643e6c6b6947d2827d8216c9642587
XuZeJun <xuzejun@126.com>
2019-11-13 11:41:50 +0800

    提交新功能

---
+++ gmall-manage-web/src/test/java/com/atguigu/gmall/manage/GmallManageWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.manage;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class GmallManageWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
